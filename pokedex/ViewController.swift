7d84c33a4fac4b039523bde678a7ac067e7625c2
Quang <qilytquang@gmail.com>
2016-12-18 06:08:19 -0800

    added collection view delegate, datasource, flow layout

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -8,14 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    //var pokemons = [Pokemon]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let pikachu = Pokemon(name: "Pikachu", pokedexId: 25)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        //let pikachu = Pokemon(name: "Pikachu", pokedexId: 25)
     }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = Pokemon(name: "Pokemon", pokedexId: indexPath.row + 1)
+            cell.configureCell(pokemon: pokemon)
+            
+            return cell
+        } else {
+            
+            return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 100, height: 100)
+    }
 
 }
 
