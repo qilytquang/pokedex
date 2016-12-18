213f39b6b782f824c42a133a07b7e9482b449965
Quang <qilytquang@gmail.com>
2016-12-18 04:29:16 -0800

    added class Pokemon

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Quang on 12/18/16.
+//  Copyright © 2016 Quang. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
