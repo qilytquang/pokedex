ee509659f6ce5456513123a98ab255f5c50a0a6d
Quang <qilytquang@gmail.com>
2016-12-21 06:50:47 -0800

    complete downloading and parsing data from API into app

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Quang on 12/19/16.
+//  Copyright © 2016 Quang. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- pokedex/PokemonDetailsVC.swift
+++ pokedex/PokemonDetailsVC.swift
@@ -19,7 +19,7 @@ class PokemonDetailsVC: UIViewController {
     @IBOutlet weak var defenseLbl: UILabel!
     @IBOutlet weak var heightLbl: UILabel!
     @IBOutlet weak var pokedexIdLbl: UILabel!
-    @IBOutlet weak var widthLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
     @IBOutlet weak var baseAttackLbl: UILabel!
     @IBOutlet weak var evoLbl: UILabel!
     @IBOutlet weak var currentEvoImg: UIImageView!
@@ -28,7 +28,37 @@ class PokemonDetailsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        nameLbl.text = pokemon.name
+        nameLbl.text = pokemon.name.capitalized
+        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        pokedexIdLbl.text = "\(pokemon.pokedexId)"
+        currentEvoImg.image = UIImage(named: "\(pokemon.pokedexId)")
+        
+        pokemon.downloadPokemonDetails {
+            
+            // whatever we write will only be called after the network call is complete!
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        
+        descriptionLbl.text = pokemon.description
+        typeLbl.text = pokemon.type
+        defenseLbl.text = pokemon.defense
+        heightLbl.text = pokemon.height
+        weightLbl.text = pokemon.weight
+        baseAttackLbl.text = pokemon.attack
+        
+        if pokemon.nextEvolutionId == "" {
+            
+            evoLbl.text = "No Evolutions"
+            nextEvoImg.isHidden = true
+        } else {
+            
+            evoLbl.text = "Next Evolution: \(pokemon.nextEvolutionName) - LVL \(pokemon.nextEvolutionLevel)"
+            nextEvoImg.isHidden = false
+            nextEvoImg.image = UIImage(named: pokemon.nextEvolutionId)
+        }
     }
 
     @IBAction func backBtnPressed(_ sender: UIButton) {
